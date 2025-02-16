30fca8dc1d110358edceda708897f7d2e6a52305
Yoav Uljamperl <yoavuli@gmail.com>
2025-02-16 11:33:28 +0200

    Initial commit

---
+++ app/src/main/java/com/yoavuli/imageloaderapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.yoavuli.imageloaderapp
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.yoavuli.imageloader.ImageLoader
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.title).text = ImageLoader().greet()
+        Log.w("yoavImageLoader",ImageLoader().greet())
+    }
+}
